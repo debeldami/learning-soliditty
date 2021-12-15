5446efb728f27544fee79da4aefd99cb0f070f5b
debeldami <debeldamilola@gmail.com>
2021-12-15 18:13:05 +0100

    default values

---
+++ DefaultVal.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+//solidity have default values if you don't assign values to a variable
+
+contract DefaultVal{
+                    //default value 
+    bool public b;  //false
+    uint public u;  //0
+    int public i;   //0
+    address public a; //0x0000000000000000000000000000000000000000
+    bytes32 public b32; //0x000000000000000000000000000000000000000000000000000000000000
+} 
