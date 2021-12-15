c0097f978a278eeaf322425941597ac028fddeb5
debeldami <debeldamilola@gmail.com>
2021-12-15 18:05:05 +0100

    simple counter contract

---
+++ Counter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+//a simple solidity application that increment and decrement a variable on the blockchain
+
+contract Counter{
+    uint public count;
+
+    function increment() external{
+        count = count + 1;
+    }
+
+    function decremnt() external{
+        count = count - 1;
+    }
+}
