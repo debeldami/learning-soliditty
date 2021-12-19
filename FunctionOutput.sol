1c00a250db23ce4124c2dbe15d29ac759878ecf2
debeldami <debeldamilola@gmail.com>
2021-12-19 13:23:52 +0100

    function output

---
+++ FunctionOutput.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+contract FunctionOuput{
+    function returnMany() public pure returns(uint, bool){
+        return (1, true);
+    }
+
+    function returnNamed() public pure returns(uint x, bool y){
+        return (1, true);
+    }
+
+    function returnAssigned() public pure returns(uint x, bool y){
+        x = 1;
+        y = true;
+    }
+
+    function destructuringAss() public pure{
+        (uint x, bool y) = returnMany();
+        (, bool b) = returnMany();
+    
+    }
+}
