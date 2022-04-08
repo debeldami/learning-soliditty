a02267bbf5cc9355c3231f1a8cd80dba4fbe850a
debeldami <debeldamilola@gmail.com>
2022-04-08 19:42:34 +0100

    add comment to some built in variables

--- builtInVariable.sol
+++ builtInVariable.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract BuiltInV{
-    address sender = msg.sender;
-    uint val = msg.value;
-    uint date = block.timestamp;
+    address sender = msg.sender; //the person sending the transaction
+    uint val = msg.value; //contain the value of ether in the contract call
+    uint date = block.timestamp; //contain time from the genesis block of etheruem
 }
