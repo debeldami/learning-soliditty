f1da99e8098a7c7b51b3f456ac449ffd5f032ce7
debeldami <debeldamilola@gmail.com>
2022-04-15 14:37:34 +0100

    voting v2

--- voting.sol
+++ voting.sol
@@ -1,39 +1,46 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.13;
+pragma solidity 0.8.7;
 
 contract Voting{
-
-     mapping(uint => mapping(address => bool)) vote;
-     
-     mapping(uint => address[]) votedAddress;
-
-     string[] proposal;
-
-     function makeProposal(string calldata prop) public{
-
-        require((keccak256(abi.encodePacked((prop))) != keccak256(abi.encodePacked(("")))), "give a valid proposal");
-
-         proposal.push(prop);
-     }
-
-     function castVote(uint ip, bool voteFor) public {
-
-         require((keccak256(abi.encodePacked((proposal[ip]))) != keccak256(abi.encodePacked(("")))), "no such proposal");
-         require(proposal.length <= ip, "no such proposal");
-
-         vote[ip][msg.sender] = voteFor;
-         votedAddress[ip].push(msg.sender);
-     }
-
-     function voteResult(uint ip) public view returns(uint positiveResult, uint totalVoter ){
     
-         totalVoter = votedAddress[ip].length;
+    struct Proposal {
+        uint id;
+        string proposalTitle;
+        string proposalDescription;
+        uint totalVote;
+        uint positiveVote;
+    }
 
-         for(uint i = 0; i < totalVoter; i++){
-             if(vote[ip][votedAddress[ip][i]]){
-                 positiveResult += 1;
-             }
-         }
-     }
+    mapping(uint => mapping(address => bool)) votedAddress;
+
+    Proposal[] proposal;
+
+    function createPropsal (string memory title, string memory desc) public{
+        require((keccak256(abi.encodePacked((title))) != keccak256(abi.encodePacked(("")))), "empty title not allowed");
+        require((keccak256(abi.encodePacked((desc))) != keccak256(abi.encodePacked(("")))), "empty description not allowed");
+
+        proposal.push(Proposal(proposal.length + 1, title, desc, 0, 0));
+    }   
+
+    function castVote(uint _ip, bool voteYes) public{
+
+        require(proposal.length < _ip, "no such improvements proposal");
+
+        if(votedAddress[_ip][msg.sender]){
+            revert("user already voted");
+        }else{
+            proposal[_ip].totalVote +=1;
+            if(voteYes){
+                proposal[_ip].positiveVote +=1;
+            }
+        }
+
+        votedAddress[_ip][msg.sender] = true;
+    }
+
+    function getVote(uint _ip) public view returns(uint totalVote, uint positiveVote){
+        totalVote = proposal[_ip].totalVote;
+        positiveVote = proposal[_ip].positiveVote;
+    }
 }
