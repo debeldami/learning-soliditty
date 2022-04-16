ce9efa555d05c9c5ac2e22d45878b0f8f0d45e0c
debeldami <debeldamilola@gmail.com>
2022-04-16 21:54:56 +0100

    generic commit

--- ternary.sol
+++ ternary.sol
@@ -1,7 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.13;
 
 contract Ternary{
-    function ternary(uint input) public returns(bool){
+    function ternary(uint input) public pure returns(bool){
         return input < 256 ? true : false;
     }
 }
